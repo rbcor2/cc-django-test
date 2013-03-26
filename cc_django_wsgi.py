6400d5d6e407d8330826c17c993161d0fd87c537
Martin-Zack Mekkaoui <martin@mekkaoui.fr>
2013-03-26 16:02:15 +0100

    fix startup

--- cc_django_wsgi.py
+++ cc_django_wsgi.py
@@ -3,7 +3,7 @@ import sys
 import os
  
 sys.path.append("/home/bas/app_e7a18983-405e-4ada-ac68-2738f2767461")
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cc_django.settings'
 os.environ['PYTHON_EGG_CACHE'] = '/tmp/.python-egg'
  
 import django.core.handlers.wsgi
